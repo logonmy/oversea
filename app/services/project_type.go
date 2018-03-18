4026582b908770112d07ec634229804a0146776b
471720348@qq.com <471720348@qq.com>
2018-03-18 17:14:16 +0800

    修改原生css

--- app/services/project_type.go
+++ app/services/project_type.go
@@ -5,16 +5,12 @@ import (
 	"oversea/app/entitys"
 )
 
-// AddOzProjectType insert a new OzProjectType into database and returns
-// last inserted Id on success.
 func AddOzProjectType(m *entitys.OzProjectType) (id int64, err error) {
 	o := orm.NewOrm()
 	id, err = o.Insert(m)
 	return
 }
 
-// GetOzProjectTypeById retrieves OzProjectType by Id. Returns error if
-// Id doesn't exist
 func GetOzProjectTypeById(id int) (v *entitys.OzProjectType, err error) {
 	o := orm.NewOrm()
 	v = &entitys.OzProjectType{Id: id}
