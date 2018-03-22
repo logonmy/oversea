15048dbc5aa840d251af1f595c25b0d4a5e0ff0f
weilanzhuan133 <weilanzhuan133@pingan.com.cn>
2018-03-22 17:29:18 +0800

    修改后台管理样式风格

--- static/backend/js/site.js
+++ static/backend/js/site.js
@@ -1,135 +1,142 @@
 $(function () {
+
+    //自定义正则表达示验证方法
+    $.validator.addMethod("checkMobile",function(value,element,params){
+        var phoneReg = /^1[0-9]{10}$/;
+        return this.optional(element)||(phoneReg.test(value));
+    },"*请输入正确的手机号码！");
+
+    $.validator.addMethod("checkPwd",function(value,element,params){
+        var checkPwd = /^\w{6,16}$/g;
+        return this.optional(element)||(checkPwd.test(value));
+    },"*只允许6-16位英文字母、数字或者下画线！");
+
+    $.validator.addMethod("checkEmail",function(value,element,params){
+        var checkEmail = /^[a-z0-9]+@([a-z0-9]+\.)+[a-z]{2,4}$/i;
+        return this.optional(element)||(checkEmail.test(value));
+    },"*请输入正确的邮箱！");
+
     // 保存管理员
-    $('#userForm').bootstrapValidator({
-        fields: {
+    $('#userForm').validate({
+        rules: {
             password: {
-                message: '密码无效',
-                validators: {
-                    notEmpty: {
-                        message: '密码不能为空'
-                    },
-                    stringLength: {
-                        min: 6,
-                        max: 30,
-                        message: '密码长度必须在6到30之间'
-                    },
-                    regexp: {
-                        regexp: /^[a-zA-Z0-9_\.]+$/,
-                        message: '密码只能由字母、数字、点和下划线'
-                    }
-                }
+                required:true,
+                checkPwd:true,
             },
             repassword: {
-                message: '密码无效',
-                validators: {
-                    notEmpty: {
-                        message: '确认密码不能为空'
-                    },
-                    stringLength: {
-                        min: 6,
-                        max: 30,
-                        message: '密码长度必须在6到30之间'
-                    },
-                    identical: {
-                        field: 'password',
-                        message: '两次密码不一致'
-                    },
-                    regexp: {
-                        regexp: /^[a-zA-Z0-9_\.]+$/,
-                        message: '密码只能由字母、数字、点和下划线'
-                    }
-                }
+                required:true,
+                checkPwd:true,
+                equalTo: 'input[name="password"]'
             },
             phone: {
-                message: '手机号码不能为空',
-                validators: {
-                    notEmpty: {
-                        message: '手机号码不能为空'
-                    },
-                    regexp: {
-                        regexp: /^1[0-9]{10}$/,
-                        message: '请输入正确的手机号码'
-                    }
-                }
+                required: true,
+                checkMobile:true
+            },
+            email: {
+                required: true,
+                checkEmail:true
             },
             real_name: {
-                message: '管理员名称不能为空',
-                validators: {
-                    notEmpty: {
-                        message: '管理员名称不能为空'
-                    },
-                    stringLength: {
-                        min: 2,
-                        max: 10,
-                        message: '密码长度必须在2到10之间'
-                    },
-                }
+                required: true,
+                minlength: 2,
+                maxlength: 10
             },
-            email: {
-                validators: {
-                    emailAddress: {
-                        message: 'email地址格式错误'
-                    }
-                }
+        },
+        messages: {
+            real_name: {
+                required: "管理员名称不能为空",
+                minlength: "密码长度必须在2到10之间"
             },
-        }
-    }).on('success.form.bv', function (e) {
-        e.preventDefault();
-        var $form = $(e.target);
-        $.post($form.attr('action'), $form.serialize(), function (result) {
-            if (result.errorCode != 0) {
-                toastr.error(result.errorMsg);
-                return false;
-            } else {
-                toastr.success(result.errorMsg);
-                setTimeout(function () {
-                    location.href = result.data.href
-                }, 2000)
+            phone:{
+                required: "手机号码不能为空",
+                checkMobile: "请输入正确手机号码"
+            },
+            email:{
+                required: "邮箱地址不能为空",
+                checkEmail: "请输入正确邮箱地址"
+            },
+            password:{
+                required:"请填写密码",
+                checkPwd:"密码必须是6-16位英文字母、数字或者下画线"
+            },
+            repassword:{
+                required:"请填写密码",
+                checkPwd:"密码必须是6-16位英文字母、数字或者下画线"
             }
-        }, 'json').complete(function () {
-            $('#btnAddUser').removeAttr('disabled')
-        });
+        },
+        invalidHandler: function(form, validator) {  //不通过回调
+            return false;
+        },
+        submitHandler: function (e) {
+            // e.preventDefault();
+            var $form = $(e);
+
+            $.ajax({
+                type: 'post',
+                url: $form.attr('action'),
+                data: $form.serialize(),
+                dataType : "json",
+                success: function(data) {
+                    toastr.success(result.errorMsg);
+                    setTimeout(function () {
+                        location.href = result.data.href
+                    }, 2000)
+                },
+                complete:function () {
+                    alert(111);
+                    $('button[type="submit"]').removeAttr('disabled')
+                }
+            });
+
+            return false;
+
+        },
     });
 
+
+
     // 编辑管理员-保存
-    $('#userEditForm').bootstrapValidator({
-        fields: {
+    $('#userEditForm').validate({
+        rules: {
             phone: {
-                message: '手机号码不能为空',
-                validators: {
-                    notEmpty: {
-                        message: '手机号码不能为空'
-                    },
-                    regexp: {
-                        regexp: /^1[0-9]{10}$/,
-                        message: '请输入正确的手机号码'
-                    }
-                }
+                required: true,
+                checkMobile:true
             },
             email: {
-                validators: {
-                    emailAddress: {
-                        message: 'email地址格式错误'
-                    }
-                }
+                required: true,
+                checkEmail:true
             },
-        }
-    }).on('success.form.bv', function (e) {
-        e.preventDefault();
-        var $form = $(e.target);
-        $.post($form.attr('action'), $form.serialize(), function (result) {
-            if (result.errorCode != 0) {
-                toastr.error(result.errorMsg);
-                return false;
-            } else {
-                toastr.success(result.errorMsg);
-                setTimeout(function () {
-                    location.href = result.data.href
-                }, 2000)
-            }
-        }, 'json').complete(function () {
-            $('#btnEditUser').removeAttr('disabled')
-        });
+        },
+        messages: {
+            phone:{
+                required: "手机号码不能为空",
+                checkMobile: "请输入正确手机号码"
+            },
+            email:{
+                required: "邮箱地址不能为空",
+                checkEmail: "请输入正确邮箱地址"
+            },
+        },
+        submitHandler: function (e) {
+            var $form = $(e);
+            $.ajax({
+                type: 'post',
+                url: $form.attr('action'),
+                data: $form.serialize(),
+                dataType : "json",
+                success: function(data) {
+                    toastr.success(result.errorMsg);
+                    setTimeout(function () {
+                        location.href = result.data.href
+                    }, 2000)
+                },
+                complete:function () {
+                    alert(111);
+                    $('button[type="submit"]').removeAttr('disabled')
+                }
+            });
 
+            return false
+        }
     });
 });
