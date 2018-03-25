316edb52b7a17eab5645246ed51c29fc3aa539c1
471720348@qq.com <471720348@qq.com>
2018-03-25 20:50:39 +0800

    添加前端首页模板

--- static/frontend/js/common.js
+++ static/frontend/js/common.js
@@ -1,17 +1,3 @@
 $(function () {
-    $(window).scroll(function(event){
-        var top = $("#demo2").height();
-        var scrolla=$(window).scrollTop();
-        var cha=parseInt(top)-parseInt( scrolla);
-        if(cha<=0)
-        {
-            $("#demo").addClass("navbar-fixed-top");
-        } else {
-            $("#demo").removeClass("navbar-fixed-top");
-        }
-
-    });
-
     $('.navbar-toggle').fancynav();
-
 });
