815ac0306100e2d571751069291bdd6295bbdedc
weilanzhuan133 <weilanzhuan133@pingan.com.cn>
2018-04-16 18:48:30 +0800

    fix

--- routers/router.go
+++ routers/router.go
@@ -13,7 +13,7 @@ func init() {
 	// 允许跨域
 	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
 		AllowAllOrigins:  false,
-		AllowOrigins:[]string{"http://localhost:8000"},
+		AllowOrigins:[]string{"http://localhost:8000","http://sq.yesbug.cn"},
 		AllowMethods:     []string{"PUT","POST","GET","DELETE"},
 		//AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin"},
 		ExposeHeaders:    []string{"Content-Length", " Access-Control-Request-Method", "Access-Control-Allow-Origin",
